6a76d46fe9f67bc0285c51f57c00aef2d3f77659
Ego Maragustaf <egomaragustaf@gmail.com>
2024-03-08 15:50:46 +0700

    chore: install prisma for database

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,31 @@
+import { db } from "@/lib/db";
+
 const OrganizationIdPage = () => {
-  return <div>Organization Page!</div>;
+  async function create(formdata: FormData) {
+    "use server";
+
+    const title = formdata.get("title") as string;
+
+    await db.board.create({
+      data: {
+        title,
+      },
+    });
+  }
+
+  return (
+    <div>
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
