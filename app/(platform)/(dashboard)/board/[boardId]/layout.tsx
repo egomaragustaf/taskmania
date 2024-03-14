0a4dd2a92f305889588ec6c594e0f8144aee5e2f
Ego Maragustaf <egomaragustaf@gmail.com>
2024-03-14 16:02:54 +0700

    feat(ui): add navbar board organizationId page

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -3,6 +3,8 @@ import { notFound, redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
 
+import { BoardNavbar } from "./_components/board-navbar";
+
 export async function generateMetadata({
   params,
 }: {
@@ -56,6 +58,8 @@ const BoardIdLayout = async ({
     <div
       className="relative h-full bg-no-repeat bg-cover bg-center"
       style={{ backgroundImage: `url(${board.imageThumbUrl})` }}>
+      <BoardNavbar data={board} />
+      <div className="absolute inset-0 bg-black/10" />
       <main className="relative pt-28 h-full">{children}</main>
     </div>
   );
