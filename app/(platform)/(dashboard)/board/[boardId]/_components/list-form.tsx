92aa74d8211c0a65689a4623ca83b7052f5fc1e0
Ego Maragustaf <egomaragustaf@gmail.com>
2024-03-15 15:39:38 +0700

    feat: add list board with server action

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -1,16 +1,21 @@
 "use client";
 
+import { toast } from "sonner";
 import { Plus, X } from "lucide-react";
 import { useEventListener, useOnClickOutside } from "usehooks-ts";
 import { useState, useRef, ElementRef } from "react";
+import { useParams, useRouter } from "next/navigation";
 
+import { useAction } from "@/hooks/use-action";
+import { createList } from "@/actions/create-list";
 import { FormInput } from "@/components/form/form-input";
-import { ListWrapper } from "./list-wrapper";
-import { useParams } from "next/navigation";
 import { FormSubmit } from "@/components/form/form-button";
 import { Button } from "@/components/ui/button";
 
+import { ListWrapper } from "./list-wrapper";
+
 export const ListForm = () => {
+  const router = useRouter();
   const params = useParams();
 
   const formRef = useRef<ElementRef<"form">>(null);
@@ -29,6 +34,17 @@ export const ListForm = () => {
     return setIsEditing(false);
   };
 
+  const { execute, fieldErrors } = useAction(createList, {
+    onSuccess: (data) => {
+      toast.success(`List "${data.title}" created!`);
+      disableEditing();
+      router.refresh();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
   const onKeyDown = (e: KeyboardEvent) => {
     if (e.key === "Escape") {
       disableEditing();
@@ -38,14 +54,23 @@ export const ListForm = () => {
   useEventListener("keydown", onKeyDown);
   useOnClickOutside(formRef, disableEditing);
 
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const boardId = formData.get("boardId") as string;
+
+    execute({ title, boardId });
+  };
+
   if (isEditing) {
     return (
       <ListWrapper>
         <form
+          action={onSubmit}
           ref={formRef}
           className="w-full p-3 rounded-md bg-white space-y-4 shadow-md">
           <FormInput
             ref={inputRef}
+            errors={fieldErrors}
             id="title"
             className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition"
             placeholder="Enter list title..."
